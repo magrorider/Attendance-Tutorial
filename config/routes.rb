0b9091f61154e312446ec09151a4c9f88a5088a6
koyama <a1989k04i24o@yahoo.co.jp>
2019-07-16 16:56:38 +0000

    Finish add-basic-info

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
 
   # get 'users/new'
 
-  resources :users
+  resources :users do
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+    end
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
