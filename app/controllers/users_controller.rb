36495323cbac18c5f58252eb06558f4a1b03b653
koyama <a1989k04i24o@yahoo.co.jp>
2019-08-01 15:03:46 +0000

    Finish edit-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,26 +17,6 @@ class UsersController < ApplicationController
     @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
   end
 
-  def destroy
-    @user.destroy
-    flash[:success] = "#{@user.name}のデータを削除しました。"
-    redirect_to users_url
-  end
-  
-  def edit_basic_info
-  end
-
-  def update_basic_info
-    if @user.update_attributes(basic_info_params)
-      # 更新成功時の処理
-      flash[:success] = "#{@user.name}の基本情報を更新しました。"
-    else
-      # 更新失敗時の処理
-      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
-    end
-    redirect_to users_url
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -61,6 +41,26 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
+  
+  def edit_basic_info
+  end
+  
+  def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      # 更新成功時の処理
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      # 更新失敗時の処理
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+    redirect_to users_url
+  end
+  
   private
 
     def user_params
@@ -69,32 +69,6 @@ class UsersController < ApplicationController
     
     def basic_info_params
         params.require(:user).permit(:department, :basic_time, :work_time)
-    end
-    
-    # beforeフィルター
-    
-    # paramsハッシュからユーザーを取得します。
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # ログイン済みのユーザーか確認します。
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
-    
-    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
-    def correct_user
-      redirect_to(root_url) unless current_user?(@user)
-    end
-
-    # システム管理権限所有かどうか判定します。
-    def admin_user
-      redirect_to root_url unless current_user.admin?
     end
 
 end
